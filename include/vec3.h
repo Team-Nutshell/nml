89c0f3a9acb09ef728f19c9a0b8fb0c0e6356189
ZaOniRinku <zaonirinku@gmail.com>
2022-11-17 12:46:22 +0100

    add reflect and refract functions and change operators

--- include/vec3.h
+++ include/vec3.h
@@ -12,12 +12,12 @@ struct vec3 {
 	
 	// Constructors
 	vec3();
+	vec3(float _xyz);
 	vec3(float _x, float _y, float _z);
 	vec3(float _x, vec2 _yz);
 	vec3(vec2 _xy, float _z);
 
 	// Operators
-	inline vec3 operator+(const vec3& other) { return vec3(x + other.x, y + other.y, z + other.z); }
 	vec3& operator+=(const vec3& other) { 
 		x += other.x;
 		y += other.y;
@@ -25,7 +25,6 @@ struct vec3 {
 
 		return *this;
 	}
-	inline vec3 operator-(const vec3& other) { return vec3(x - other.x, y - other.y, z - other.z); }
 	vec3& operator-=(const vec3& other) { 
 		x -= other.x;
 		y -= other.y;
@@ -33,17 +32,43 @@ struct vec3 {
 
 		return *this;
 	}
-	inline bool operator==(const vec3& other) { return ((x == other.x) && (y == other.y) && (z == other.z)); }
-	inline bool operator!=(const vec3& other) { return !(*this == other); }
+	vec3& operator*=(const float other) {
+		x *= other;
+		y *= other;
+		z *= other;
+
+		return *this;
+	}
+	vec3& operator/=(const float other) {
+		x /= other;
+		y /= other;
+		z /= other;
+
+		return *this;
+	}
 
 	// Functions
 	float length();
 	vec3 normalize();
 
+	float* data();
 	std::string to_string();
 };
 
+// Operators
+inline vec3 operator+(vec3 lhs, const vec3& rhs) { return vec3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z); }
+inline vec3 operator-(vec3 lhs, const vec3& rhs) { return vec3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z); }
+inline vec3 operator*(vec3 lhs, const float rhs) { return vec3(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs); }
+inline vec3 operator*(float lhs, const vec3& rhs) { return rhs * lhs; }
+inline vec3 operator/(vec3 lhs, const float rhs) { return vec3(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs); }
+inline vec3 operator/(float lhs, const vec3& rhs) { return rhs / lhs; }
+inline bool operator==(const vec3& lhs, const vec3& rhs) { return ((lhs.x == rhs.x) && (lhs.y == rhs.y) && (lhs.z == rhs.z)); }
+inline bool operator!=(const vec3& lhs, const vec3& rhs) { return !(lhs == rhs); }
+
+// Functions
 float dot(const vec3& a, const vec3& b);
 vec3 cross(const vec3& a, const vec3& b);
+vec3 reflect(const vec3& i, const vec3& n);
+vec3 refract(const vec3& i, const vec3& n, float eta);
 
 }
--- src/vec2.cpp
+++ src/vec2.cpp
@@ -4,6 +4,7 @@
 namespace ntshm {
 
 vec2::vec2(): x(0.0f), y(0.0f) {}
+vec2::vec2(float _xy): x(_xy), y(_xy) {}
 vec2::vec2(float _x, float _y): x(_x), y(_y) {}
 
 float vec2::length() {
@@ -17,12 +18,31 @@ vec2 vec2::normalize() {
 		y / l);
 }
 
+float* vec2::data() {
+	return &x;
+}
+
 std::string vec2::to_string() {
 	return (std::to_string(x) + ", " + std::to_string(y));
 }
 
 float dot(const vec2& a, const vec2& b) {
 	return ((a.x * b.x) + (a.y * b.y));
+}
+
+vec2 reflect(const vec2& i, const vec2& n) {
+	return (i - 2.0f * dot(n, i) * n);
+}
+
+vec2 refract(const vec2& i, const vec2& n, float eta) {
+	float ndoti = dot(n, i);
+	float k = 1.0f - eta * eta * (1.0f - ndoti * ndoti);
+	if (k < 0.0f) {
+		return vec2(0.0f);
+	}
+	else {
+		return eta * i - (eta * ndoti + std::sqrt(k)) * n;
+	}
 }
 
 }
